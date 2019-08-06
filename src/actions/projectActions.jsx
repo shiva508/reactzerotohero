62deafd8354175efcb4ad0bdbe2f6ef78c369020
shiva508 <dasarishiva1@gamil.com>
2019-08-06 16:15:07 +0530

    bvgxcb

--- src/actions/projectActions.jsx
+++ src/actions/projectActions.jsx
@@ -1,1 +1,10 @@
-import axios from 'axios'
+import axios from 'axios';
+import {GET_ERROR} from './types';
+import { async } from 'q';
+export const createProject=(project,histry)=async dispatch=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
--- src/reducers/errorReducer.jsx
+++ src/reducers/errorReducer.jsx
@@ -1,11 +1,11 @@
-import {GET_ERROR} from '../actions';
+import {GET_ERROR} from '../actions/types';
 const initialState={};
 export default function(state=initialState,action){
     switch(action.type){
        case GET_ERROR:
-           action.payload;
+          return action.payload;
 
         default:
-            return state
+            return state;
     }
 }
